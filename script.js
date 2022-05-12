abd073aa34f32e96ddc1589aeab6abc2acd7bf57
Sebastian <s.romhany@gmail.com>
2022-05-12 12:22:53 -0400

    Most of the interface done as well as game functionality. Just missing wins conditions and game reset functions

--- script.js
+++ script.js
@@ -3,23 +3,77 @@
 const boardElement = document.querySelector('#board');
 
 
-
-const gameboard = function(){
-    let gameState = {a1:'x', a2:'o',a3:'x',b1:'x',b2:'o',b3:'x',c1:'o',c2:'x',c3:'o'};
-    const updateBoard = function (){
-        for (const key in gameState) {
+const board = function(){
+    let gameState = ['','','','','','','','','']
+    const update = function (){
+        while (boardElement.firstChild) {
+            boardElement.removeChild(boardElement.lastChild);
+          }
+        for (let i = 0; i < gameState.length; i++) {
             let square = document.createElement('div');
-            square.setAttribute('data-square',`${key}`);
             square.classList.add('square');
-            square.textContent = `${gameState[key]}`;
+            if (gameState[i] !== '') {
+                square.classList.add(`${gameState[i]}`)}
+            square.setAttribute('data-index',`${i}`);
+            square.setAttribute('data-xoxo',`${gameState[i]}`)
+
             boardElement.appendChild(square);
+            square.addEventListener('click',game.playerTurn)
+            
         }
+        checkWinCondtion();
+    }
+    function addPlay (str,index){
+        gameState[index] = str;
+        board.update();
         
     }
-    return {updateBoard}
+        function checkWinCondtion(){
+            for (let i = 0; i < 3; i++){
+                if ((gameState[i] !== '' && gameState[i] === gameState[i+1] && gameState[i+1] === gameState[i+2])
+                    || (gameState[i] !== '' && gameState[i] === gameState[i+3] && gameState[i+3] === gameState[i+6])
+                    || (gameState[0] !== '' && gameState[0] === gameState[4] && gameState[4] === gameState[8])
+                    || (gameState[2] !== '' && gameState[2] === gameState[4] && gameState[4] === gameState[6])) {
+                    gameOver
+                    return console.log('last player wins')
+                }
+            }
+            if (gameState.find(str=>str == '') == undefined) {
+                /* game ends */return alert('Tie')
+            }
+        }
+    return {update,addPlay,gameState}
+}()
+
+const game = function(){
+    let turn = true; /* true = X turn, false = O turn */
+    const changeturn = function (){
+        if (turn === false){turn = true } 
+            else {turn = false}
+    }
+    const playerTurn = function() {
+        if (this.dataset.xoxo !== '') {
+            return
+        } ;
+        let tag = this.dataset.square;
+        let index = this.dataset.index;
+        let choice = '';
+        if (turn === true) {
+            choice = 'x'
+            changeturn()} else if (turn === false){
+                choice = 'o';
+                changeturn()
+            }
+        board.addPlay(choice,index)
+        
+        }
+    
+    return {playerTurn}
 }()
 const players = [];
+
 function newPlayer (name){
-    return {name, wins:0, gamesPlayed:0, streak:0}
+    players.push({name, wins:0, gamesPlayed:0, streak:0});
 }
 
+board.update()
